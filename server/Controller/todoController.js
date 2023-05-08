75b57d9d374feff47e15340926fd9468fd656baa
mayank kumar ratre <mayankratre10@gmail.com>
2023-05-09 00:10:17 +0530

    Access-control-Allow-Origin: true

--- server/Controller/todoController.js
+++ server/Controller/todoController.js
@@ -4,6 +4,7 @@ const getTodos = async (req, res) => {
     try {
         let todos = await todoModel.find({});
         console.log(todos);
+        res.setheader("Access-Control-Allow-Origin","true" );
         res.send(todos);
     }
     catch (err) {
@@ -15,7 +16,8 @@ const addTodos = async (req, res) => {
     try {
         let mytodo = req.body.mytodo;
         await todoModel.create(mytodo);
-        res.json({message:"todo ADDED"});
+        // res.setheader("Access-Control-Allow-Origin","true" );
+        res.send("ADDED");
     }
     catch (err) {
         console.log(err.message);
@@ -26,6 +28,7 @@ const deleteTodos = async (req, res) => {
     try {
         let sn = req.body.S_no;
         await todoModel.findOneAndDelete({S_no:sn}).catch((err)=>{console.log(err.message)});
+        // res.setheader("Access-Control-Allow-Origin","true" );
     }
     catch (err) {
         console.log(err.message);

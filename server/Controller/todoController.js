0d30aa89fa3827a83652a9ffedd4e117d3491285
Mayank Kumar Ratre <82997237+MAYANKRATRE10@users.noreply.github.com>
2023-05-09 00:56:10 +0530

    Update todoController.js
    NO neet of setHeader

--- server/Controller/todoController.js
+++ server/Controller/todoController.js
@@ -4,7 +4,6 @@ const getTodos = async (req, res) => {
     try {
         let todos = await todoModel.find({});
         console.log(todos);
-        res.setheader("Access-Control-Allow-Origin","true" );
         res.send(todos);
     }
     catch (err) {
@@ -16,7 +15,6 @@ const addTodos = async (req, res) => {
     try {
         let mytodo = req.body.mytodo;
         await todoModel.create(mytodo);
-        // res.setheader("Access-Control-Allow-Origin","true" );
         res.send("ADDED");
     }
     catch (err) {
@@ -28,7 +26,6 @@ const deleteTodos = async (req, res) => {
     try {
         let sn = req.body.S_no;
         await todoModel.findOneAndDelete({S_no:sn}).catch((err)=>{console.log(err.message)});
-        // res.setheader("Access-Control-Allow-Origin","true" );
     }
     catch (err) {
         console.log(err.message);
